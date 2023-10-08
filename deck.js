918d8968fc00dc1a71ca0743a85cf32531788d3e
elugo33 <140855002+elugo33@users.noreply.github.com>
2023-10-08 10:22:50 -0500

    final commit

--- deck.js
+++ deck.js
@@ -1,46 +1,70 @@
-const SUITS=["♠", "♥", "♣","♦"]
-const VALUES=["A", "2","3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-export default class Deck{
-    constructor(cards = freshDeck()){
-        this.cards= []
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
 
-        get NumberOfCards( {
-            return this.cards.length
-        })
-shuffle(){
-   for (let i = this.NumberOfCards - 1; i>0; i--) {
-    const newIndex = Math.floor(Math.random() * (i+i))
-    const OldValue = this.cards[newIndex]
-    this.cards[newIndex] = this.cards[i]
-    this.cards[i] = OldValue
-   }
-}
+export default class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards
+  }
+
+  get numberOfCards() {
+    return this.cards.length
+  }
+
+  pop() {
+    return this.cards.shift()
+  }
+
+  push(card) {
+    this.cards.push(card)
+  }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1))
+      const oldValue = this.cards[newIndex]
+      this.cards[newIndex] = this.cards[i]
+      this.cards[i] = oldValue
     }
+  }
 }
-class Card{
-    constructor(suit,value){
-        this.suit=suit
-        this.value=value
-    }
-    get color()
-return this.suit === '♠' || this.suit==='♣' ? 'black': 'red'
+
+class Card {
+  constructor(suit, value) {
+    this.suit = suit
+    this.value = value
+  }
+
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement("div")
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add("card", this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
 }
-getHTML() {
-    const cardDiv= document.createElement('div')
-    cardDiv.innerText=this.suit
-    cardDiv.classList.add ("card", this.color)
-}
-}
-function freshDeck(){
-    return SUITS.flatMap(suit =>{
-        return VALUES.map(value =>{
-            return new Card (suit, value)
-        })
+
+function freshDeck() {
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value)
     })
-    
+  })
 }
-
-
-
-cardDiv.dataset.value = `${this.value} ${this.suit}`
-return cardDiv
